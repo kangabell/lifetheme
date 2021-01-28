a4b4abd2be0dc4352a1d8d3d61b9bdcb6f97de91
kangabell <k@kangabell.co>
2021-01-28 16:44:25 -0500

    don't mention comments on archive pages

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -57,25 +57,6 @@ if ( ! function_exists( 'life_entry_footer' ) ) :
 
 		}
 
-		if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
-			echo '<span class="comments-link">';
-			comments_popup_link(
-				sprintf(
-					wp_kses(
-						/* translators: %s: post title */
-						__( 'Leave a Comment<span class="screen-reader-text"> on %s</span>', 'life' ),
-						array(
-							'span' => array(
-								'class' => array(),
-							),
-						)
-					),
-					wp_kses_post( get_the_title() )
-				)
-			);
-			echo '</span>';
-		}
-
 		edit_post_link(
 			sprintf(
 				wp_kses(
