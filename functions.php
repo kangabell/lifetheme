6fcbeb0ae3016e868a314a9704b268170a5b310c
kangabell <k@kangabell.co>
2021-01-27 19:13:43 -0500

    remove woocommerce

--- functions.php
+++ functions.php
@@ -268,10 +268,3 @@ require get_template_directory() . '/inc/customizer.php';
 if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
-
-/**
- * Load WooCommerce compatibility file.
- */
-if ( class_exists( 'WooCommerce' ) ) {
-	require get_template_directory() . '/inc/woocommerce.php';
-}
