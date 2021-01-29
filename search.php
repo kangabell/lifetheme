50d87f0a731ea9e75784fa5891e82eba7ce86d1f
kangabell <k@kangabell.co>
2021-01-29 14:40:57 -0500

    search results are displayed in card format

--- search.php
+++ search.php
@@ -26,14 +26,10 @@ get_header();
 			<?php
 			/* Start the Loop */
 			while ( have_posts() ) :
+
 				the_post();
 
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', 'search' );
+				get_template_part( 'partials/card' );
 
 			endwhile;
 
