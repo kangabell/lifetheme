04e2d046fe04f2ad942ee3d0dc6b47918a83e9f2
kangabell <k@kangabell.co>
2021-01-29 14:14:16 -0500

    update full-width featured-image classes for single Projects and Characters

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -91,7 +91,7 @@ if ( ! function_exists( 'life_post_thumbnail' ) ) :
 		if ( is_singular() ) :
 			?>
 
-			<div class="post-thumbnail">
+			<div class="featured-image">
 				<?php the_post_thumbnail(); ?>
 			</div><!-- .post-thumbnail -->
 
