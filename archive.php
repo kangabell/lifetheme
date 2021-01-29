9c0ef2379acecb198c4fea4baf338350d9c2f073
kangabell <k@kangabell.co>
2021-01-29 14:53:41 -0500

    adjust which snippet layout is used for each archive page

--- archive.php
+++ archive.php
@@ -26,12 +26,13 @@ get_header();
 			while ( have_posts() ) :
 				the_post();
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+				if ( 'post' === get_post_type() ) :
+					get_template_part( 'template-parts/content' );
+				elseif ( 'life_character' === get_post_type() ) :
+					get_template_part( 'partials/thumbnail' );
+				else :
+					get_template_part( 'partials/card' );
+				endif;
 
 			endwhile;
 
