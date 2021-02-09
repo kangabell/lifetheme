711dc1907443f0f7d473c85154d829ca5362925d
kangabell <k@kangabell.co>
2021-02-09 15:47:30 -0500

    copy index.php to use for blog page

---
+++ home.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * The template for displaying the blog page
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Life
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+
+		<?php
+		if ( have_posts() ) :
+
+			if ( is_home() ) :
+				?>
+				<header class="page-header">
+					<?php
+					dynamic_sidebar( 'header-posts' );
+					?>
+				</header>
+				<?php
+			endif;
+
+			/* Start the Loop */
+			while ( have_posts() ) :
+				the_post();
+
+				/*
+				 * Include the Post-Type-specific template for the content.
+				 * If you want to override this in a child theme, then include a file
+				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+				 */
+				get_template_part( 'template-parts/content', get_post_type() );
+
+			endwhile;
+
+			the_posts_navigation( array(
+				'prev_text' => __( '<span class="icon-arrow icon-arrow-left"></span><span class="screen-reader-text">Older Posts</span>' ),
+				'next_text' => __( '<span class="screen-reader-text">Newer Posts</span><span class="icon-arrow"></span>' ),
+			));
+
+		else :
+
+			get_template_part( 'template-parts/content', 'none' );
+
+		endif;
+		?>
+
+	</main><!-- .site-main -->
+
+<?php
+get_footer();
