bec1fadf51d34405da1512bcabc28d81cec9e9bd
kangabell <k@kangabell.co>
2021-02-17 17:26:59 -0500

    fix broken search form

--- searchform.php
+++ searchform.php
@@ -1,8 +1,8 @@
 <form class="search-form" role="search" method="get" action="<?php echo home_url( '/' ); ?>">
 	<label class="screen-reader-text" for="s"><?php echo _x( 'Search for:', 'label' ) ?></label>
-	<input type="search" class="search-field" value="<?php echo get_search_query() ?>" id="s" placeholder="<?php echo esc_attr_x( 'Search', 'label' ) ?>" />
+	<input type="search" class="search-field" value="<?php echo get_search_query() ?>" id="s" placeholder="<?php echo esc_attr_x( 'Search', 'label' ) ?>" name="s" />
 	<button type="submit" class="icon-button">
 		<span class="icon-search"></span>
-		<span class="screen-reader-text">Submit</span>
+		<span class="screen-reader-text"><?php esc_html_e( 'Submit', 'life' ); ?></span>
 	</button>
 </form>
