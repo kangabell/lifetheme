988e3a6b367d5124dc86d41bf7611f5bc6e51978
kangabell <k@kangabell.co>
2021-02-23 16:36:34 -0500

    customize 404 page

--- 404.php
+++ 404.php
@@ -13,12 +13,17 @@ get_header();
 	<main id="primary" class="site-main">
 
 		<section class="error-404 not-found">
+
+			<div class="featured-image">
+				<img src="<?php echo get_template_directory_uri() . '/library/404.jpg'; ?>" alt="404 Error" />
+			</div>
+
 			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'life' ); ?></h1>
+				<h1 class="page-title"><?php esc_html_e( 'This is unfortunate.', 'life' ); ?></h1>
 			</header><!-- .page-header -->
 
 			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Please try again.', 'life' ); ?></p>
+				<p><?php esc_html_e( 'The page you are looking for is not in service. You might try using the search box. Additionally you can drop a note and let me know where things went wrong!', 'life' ); ?></p>
 			</div><!-- .page-content -->
 		</section><!-- .error-404 -->
 
