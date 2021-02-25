f09733053c30563b12c192befc9d5e266b75b234
kangabell <k@kangabell.co>
2021-02-24 20:44:12 -0500

    allow alt tag to be displayed on post thumbnail on blog page as well

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -107,18 +107,7 @@ if ( ! function_exists( 'life_post_thumbnail' ) ) :
 		<?php else : ?>
 
 			<a class="post-thumbnail" href="<?php the_permalink(); ?>" aria-hidden="true" tabindex="-1">
-				<?php
-					the_post_thumbnail(
-						'post-thumbnail',
-						array(
-							'alt' => the_title_attribute(
-								array(
-									'echo' => false,
-								)
-							),
-						)
-					);
-				?>
+				<?php the_post_thumbnail(); ?>
 			</a>
 
 			<?php
