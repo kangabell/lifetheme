eea405a977430540c089d03cf2320ab7fc35f9d4
kangabell <k@kangabell.co>
2021-03-24 11:30:08 -0400

    remove unused custom body class function

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -6,22 +6,6 @@
  */
 
 /**
- * Adds custom classes to the array of body classes.
- *
- * @param array $classes Classes for the body element.
- * @return array
- */
-function life_body_classes( $classes ) {
-	// Adds a class of hfeed to non-singular pages.
-	if ( ! is_singular() ) {
-		$classes[] = 'hfeed';
-	}
-
-	return $classes;
-}
-add_filter( 'body_class', 'life_body_classes' );
-
-/**
  * Add a pingback url auto-discovery header for single posts, pages, or attachments.
  */
 function life_pingback_header() {
