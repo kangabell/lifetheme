3fee63c246f8d59e301c0852a5f844c0f9a56bb4
kangabell <k@kangabell.co>
2021-03-30 16:15:40 -0400

    remove custom header text color

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -41,37 +41,17 @@ if ( ! function_exists( 'life_header_style' ) ) :
 	 * @see life_custom_header_setup().
 	 */
 	function life_header_style() {
-		$header_text_color = get_header_textcolor();
-
-		/*
-		 * If no custom options for text are set, let's bail.
-		 * get_header_textcolor() options: Any hex value, 'blank' to hide text. Default: add_theme_support( 'custom-header' ).
-		 */
-		if ( get_theme_support( 'custom-header', 'default-text-color' ) === $header_text_color ) {
-			return;
-		}
-
-		// If we get this far, we have custom styles. Let's do this.
 		?>
 		<style type="text/css">
-		<?php
-		// Has the text been hidden?
-		if ( ! display_header_text() ) :
-			?>
-			.site-title,
-			.site-description {
-				position: absolute;
-				clip: rect(1px, 1px, 1px, 1px);
-				}
 			<?php
-			// If the user has set a custom color for the text use that.
-		else :
-			?>
-			.site-title a,
-			.site-description {
-				color: #<?php echo esc_attr( $header_text_color ); ?>;
-			}
-		<?php endif; ?>
+			// Has the text been hidden?
+			if ( ! display_header_text() ) :
+				?>
+				.site-description {
+					position: absolute;
+					clip: rect(1px, 1px, 1px, 1px);
+				}
+			<?php endif; ?>
 		</style>
 		<?php
 	}
