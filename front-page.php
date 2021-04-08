318e192b20d1fd303c35e2ca4de033c895a5c1ab
kangabell <k@kangabell.co>
2021-04-08 17:28:05 -0400

    add list of latest projects to homepage

--- front-page.php
+++ front-page.php
@@ -21,6 +21,26 @@ get_header();
 		endwhile; // End of the loop.
 		?>
 
+		<div class="latest-content">
+			<?php
+			$query = new WP_Query( array(
+				'post_type' => 'life_project',
+				'posts_per_page' => 3,
+			) );
+			?>
+
+			<?php
+			if ( $query->have_posts() ) :
+				while ( $query->have_posts() ) : $query->the_post();
+					get_template_part( 'template-parts/card' );
+				endwhile;
+			endif;
+			wp_reset_query();
+
+			echo '<nav class="navigation archive-link"><a href="' . get_post_type_archive_link( 'life_project' ) . '"><span class="nav-title">' . esc_html__( 'View Projects', 'life' ) . '</span> <span class="icon-arrow icon-arrow-right" aria-hidden="true"></span></a></nav>';
+			?>
+		</div> <!-- .latest-content -->
+
 		<div class="secondary-content">
 			<?php dynamic_sidebar( 'home-secondary' ); ?>
 		</div>
