3d64dd312cd8d8cdccc5d85645354da421783ea5
kangabell <k@kangabell.co>
2022-01-06 15:33:37 -0500

    add 'currently listening' to rss feed content

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -25,23 +25,42 @@ add_action( 'wp_head', 'life_pingback_header' );
 
 
 /**
- * Prepend WordPress RSS feed content with the featured image
+ * Add featured image and "currently listening" to RSS feed content
  */
-function life_rss_feed_img( $content ) {
+function life_rss_feed_content( $content ) {
 
 	global $post;
 
+	$prepend = null;
+	$append = null;
+
 	if ( is_feed() ) {
+
 		if ( has_post_thumbnail( $post->ID ) ) {
+
 			$prepend = '<div>' . get_the_post_thumbnail( $post->ID, 'medium', array( 'style' => 'margin-bottom: 1rem;' ) ) . '</div>';
-			$content = $prepend . $content;
 		}
+
+		if ( function_exists( 'get_field' ) ) {
+			$artist = get_field('song_artist');
+			$song = get_field('song_link');
+
+			if ( $song ):
+				$song_url = $song['url'];
+				$song_title = $song['title'];
+				$song_target = $song['target'] ? $song['target'] : '_self';
+				$append = '<p class="song-link"><span class="label">' . esc_html__( 'Currently Listening: ', 'life' ) . '“<a class="button" href="' . esc_url( $song_url ) . '" target="' . esc_attr( $song_target ) . '">' . esc_html( $song_title ) . '</a>” ' . $artist . '</p>';
+			endif;
+		}
+
+		$content = $prepend . $content . $append;
+
 	}
 
 	return $content;
 
 }
-add_filter('the_content', 'life_rss_feed_img');
+add_filter('the_content', 'life_rss_feed_content');
 
 
 /**
