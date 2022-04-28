0ec2f142ea5e855db5e9ba215e554464129803c2
kangabell <k@kangabell.co>
2022-04-27 20:37:54 -0400

    take Edie out of the 404 page and make it more generic

--- 404.php
+++ 404.php
@@ -14,16 +14,12 @@ get_header();
 
 		<section class="error-404 not-found">
 
-			<div class="featured-image">
-				<img src="<?php echo get_template_directory_uri() . '/library/404.jpg'; ?>" alt="404 Error" />
-			</div>
-
 			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'This is unfortunate.', 'life' ); ?></h1>
+				<h1 class="page-title"><?php esc_html_e( 'Error 404', 'life' ); ?></h1>
 			</header><!-- .page-header -->
 
 			<div class="page-content">
-				<p><?php esc_html_e( 'The page you are looking for is not in service. You might try using the search box. Additionally you can drop a note and let me know where things went wrong!', 'life' ); ?></p>
+				<p><?php esc_html_e( 'Sorry, the page you are looking for is not in service. You might try using the search box.', 'life' ); ?></p>
 			</div><!-- .page-content -->
 		</section><!-- .error-404 -->
 
